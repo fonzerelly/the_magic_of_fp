c9fab3cc563a7601b8681cbb7b43a5c14f60e871
fonzerelly <ChristianHoerauf@web.de>
2015-04-25 22:35:00 +0200

    Introduce livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    livereload = require('gulp-livereload');
 
-gulp.task('default', function () {
-  console.log("Hello Gulp");
+var talk = ['index.html', 'images/*'];
+
+gulp.task('webserver', function () {
+  connect.server({
+    livereload: true
+  });
 });
+gulp.task('update', function () {
+  gulp.src(talk)
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  var server = livereload();
+  gulp.watch(talk, ['update']);
+});
+gulp.task('default', ['webserver', 'update', 'watch']);
